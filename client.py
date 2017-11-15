ca8b67301ce1f28a41de422bb6c54110696b9631
spenceryu <syu@Spencers-MBP-3.wv.cc.cmu.edu>
2017-11-15 18:54:07 -0500

    Socket: zmq.pair

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 # https://www.digitalocean.com/community/tutorials/how-to-work-with-the-zeromq-messaging-library
 import zmq
 import sys
+from datetime import datetime
 
 # ZeroMQ Context
 context = zmq.Context()
 
 # Define the socket using the "Context"
-sock = context.socket(zmq.REQ)
+#sock = context.socket(zmq.REQ) # blocked until response recv
+sock = context.socket(zmq.PAIR)
 sock.connect("tcp://127.0.0.1:5678")
 
 # Send a "message" using the socket
-sock.send(" ".join(sys.argv[1:]))
+# TODO: set up a timer for this
+#sock.send(" ".join(sys.argv[1:]))
+msg = str(datetime.now())
+sock.send_string("hi")
+
 print(sock.recv())
+
+count = 0
+while True:
+    count += 1
+    sock.send_string("HI %d" % count)
--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 import zmq
+from datetime import datetime
+
+print("Server started")
 
 # ZeroMQ Context
 context = zmq.Context()
 
 # Define the socket using the "Context"
-sock = context.socket(zmq.REP)
+# sock = context.socket(zmq.REP) # block on recv unless it has recv'd requests
+sock = context.socket(zmq.PAIR)
 sock.bind("tcp://127.0.0.1:5678")
+print("Socket bound")
 
 # Run a simple "Echo" server
 while True:
     message = sock.recv()
-    sock.send("Echo: " + message)
-    print("Echo: " + message)
+    #sock.send("Echo: " + message)
+    #print(str(datetime.now()))
+    sock.send_string("Echo: " + str(message))
+    #sock.send_string(str(datetime.now()))
+    print("Recv: " + str(message))
