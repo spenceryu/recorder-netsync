9921e1d811e6182dd9c7a37a41745ca64cec132f
spenceryu <syu@Spencers-MacBook-Pro-3.local>
2018-02-23 02:43:17 -0500

    notes about how to use pubsub

--- pair_test/pubsub/publisher.py
+++ pair_test/pubsub/publisher.py
@@ -2,7 +2,7 @@ import zmq
 
 context = zmq.Context()
 sock = context.socket(zmq.PUB)
-sock.bind("tcp://127.0.0.1:8888")
+sock.bind("tcp://127.0.0.1:8888") # sudo ifconfig to find IP address of the host pi
 
 while True:
     sock.send_string("message from publisher")
